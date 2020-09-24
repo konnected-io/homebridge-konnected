e8c9f197423f880cb3e0e07d71f17915a130b795
Mike Kormendy <mike@somethinginteractive.com>
2020-09-24 00:56:05 -0500

    Adds constants file to the repo.

---
+++ src/constants.ts
@@ -0,0 +1,44 @@
+/**
+ * For Pro Panel
+ */
+export const ZONES = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  'alarm1',
+  'out1',
+  'alarm2_out2',
+  'out',
+];
+
+/**
+ * For V1/V2 Panels
+ */
+export const PIN_TO_ZONE = {
+  1: 1,
+  2: 2,
+  5: 3,
+  6: 4,
+  7: 5,
+  8: 'out',
+  9: 6
+};
+
+export const ZONE_TO_PIN = {
+  '1': 1,
+  '2': 2,
+  '3': 5,
+  '4': 6,
+  '5': 7,
+  'out': 8,
+  '6': 9,
+};
