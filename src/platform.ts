8a1b6276c19d0d5142a76b01b7f498c1c74cfba2
Mike Kormendy <mike@somethinginteractive.com>
2020-11-02 01:54:28 -0600

    Removes the pin to zones constant import since it was removed from the constants.ts file.

--- src/platform.ts
+++ src/platform.ts
@@ -1,7 +1,7 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
 
 import { PLATFORM, PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ZONES, PIN_TO_ZONE, ZONE_TO_PIN } from './constants';
+import { ZONES, ZONES_TO_PINS, ZONE_TYPES } from './constants';
 import { KonnectedPlatformAccessory } from './platformAccessory';
 
 import client from 'node-ssdp';      // for devices discovery
